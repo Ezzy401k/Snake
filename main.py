ebb4577e8540602613cbdaf9cb9cc20879fc8ab4
Esrael Mekdem <esraelmekdem@gmail.com>
2024-03-13 12:45:22 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import turtle
+import time
+from snake import Snake
+
+screen = turtle.Screen()
+screen.setup(600, 600)
+screen.bgcolor("black")
+screen.title("My Snake Game.")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+
+    snake.move()
+
+screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,47 @@
+import turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for i in STARTING_POSITIONS:
+            new_turtle = turtle.Turtle()
+            new_turtle.color("white")
+            new_turtle.shape("square")
+            new_turtle.penup()
+            new_turtle.goto(i)
+            self.segments.append(new_turtle)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
